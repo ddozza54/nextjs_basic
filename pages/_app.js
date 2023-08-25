7fe983ad9a9ec5acce99adc3f4f55b53d57e2938
UHquokka <bananamayong@gmail.com>
2023-08-25 16:16:21 +0900

    ✨: next/head 를 이용해 head 내용 Seo.js 로 만들기

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ export default function MyApp({ Component, pageProps }) {
     // 가장 먼저 렌더링 되는 파일
     return (
         <Layout>
-            <h1>Hello</h1>
             {/* Layout.js 에서 보낸 children 이 바로 이 사이에 있는 component들이 됨. */}
             <Component {...pageProps} />
             {/* 글로벌 코드를 적용하는 법 : global 을 써준다.  */}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import NavBar from "@/components/NavBar";
+import Seo from '@/components/Seo';
+import Head from 'next/head';
+//head 컴포넌트 안에 들어가는 모든게 html head 안에 들어감
 
 export default function Home() {
     return (
         <div>
-            <NavBar />
+            <Seo title="Home" />
             <h1>Home</h1>
         </div>
 
