ccc781b1e945947a3999322f1b7b77f5af3fcebf
UHquokka <bananamayong@gmail.com>
2023-08-30 00:30:49 +0900

    📝: SSR by getServerSideProps 함수 #1

--- pages/index.js
+++ pages/index.js
@@ -2,28 +2,12 @@ import { useEffect, useState } from 'react';
 import Seo from '@/components/Seo';
 //head 컴포넌트 안에 들어가는 모든게 html head 안에 들어감
 
-export default function Home() {
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNGRlYTdkYTU2NmUzM2JhZGQ2ZTdlYWNkNGM3MjFiNCIsInN1YiI6IjY0Njg4YWRiYTUwNDZlMDEyNDY3YTQwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SHS2ccg3VvUJc0UbSNtnCxs2XEocQW0MRBRml5hNvtE'
-            }
-        };
-        (async () => {
-            const { results } = await (await fetch(`/api/movies`, options)).json()
-            setMovies(results)
-        })();
-
-    })
+export default function Home({ results }) {
     return (
         <div className='container'>
             <Seo title="Home" />
             {/* !movie 라고 하면 빈 배열이 falsy 값이 아니기 때문에 length 가 0 인 경우로 해줘야한다. */}
-            {movies.length === 0 && <h4>Loading...</h4>}
-            {movies?.map(movie =>
+            {results?.map(movie =>
                 <div key={movie.id} className="movie">
                     <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
@@ -44,5 +28,20 @@ export default function Home() {
 `}</style>
         </div>
     )
+}
 
+
+//이 함수 이름은 고정!
+//backend 에서만 동작하는 함수이기 때문에 client 에서 관여 X
+// return 한 props 는 알아서 페이지로 넘겨진다. Nextjs 가 해주는 거임. 
+// _app.js 의    <Component {...pageProps} /> 에서 이 pageProps 로 들어가는거임. 
+export async function getServerSideProps() {
+    const { results } = await
+        (await fetch(`http://localhost:3000/api/movies`)
+        ).json();
+    return {
+        props: {
+            results,
+        },
+    };
 }
