809295b2e3c6a0644b6a113a99d9451d334d5de9
UHquokka <bananamayong@gmail.com>
2023-09-01 16:35:54 +0900

    📝: url 에 id 전달하기 #1

---
+++ pages/movies/[id].js
@@ -0,0 +1,20 @@
+// moives/21412512
+
+import { useRouter } from "next/router";
+
+//url에 변수를 넣는 방법
+export default function Detail() {
+    const router = useRouter();
+    console.log(router)
+    /**
+     * query: 
+     * id: "1"
+     * url 의 정보를 찾아볼 수 있다. 
+     */
+    return (
+        <div>
+            Detail
+        </div>
+    );
+}
+
